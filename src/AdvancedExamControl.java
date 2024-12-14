ac1b8352d3f3f14ce867781e3c38766b1f5d312d
arian <arian.maki@yahoo.com>
2024-12-14 16:37:15 +0100

    standard and advanced examControl differentiation

--- src/AdvancedExamControl.java
+++ src/AdvancedExamControl.java
@@ -1,6 +1,8 @@
 // Concrete Product: Advanced ExamControl
 class AdvancedExamControl implements ExamControl {
     public void setGrade() {
+    	//it will accept a grade between 0 to 15
+    	//used for oral and written exams
         System.out.println("Setting grades with advanced control.");
     }
 }
--- src/StandardExamControl.java
+++ src/StandardExamControl.java
@@ -1,6 +1,8 @@
 // Concrete Product: Standard ExamControl
 class StandardExamControl implements ExamControl {
     public void setGrade() {
+    	//It will accept a boolean for passed or failed
+    	//it is used for presentation exams where numerical grades are not relevant
         System.out.println("Setting grades with standard control.");
     }
 }
